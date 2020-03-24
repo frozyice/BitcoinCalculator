6077f5e31347de33f2ae747863239594d292188e
Karmo <Karmo.Kiima@Eesti.ee>
2020-03-24 17:21:56 +0200

    Update calculator

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+    let currency = req.body.currency;
+    let url = `https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`;
+    let number = req.body.number;
+    
+    request(url, function(error, response, body){
+        console.log("Server status code ", response.statusCode);
+        //console.log(response);
+
+        let data = JSON.parse(response.body);
+        let price;
+        if(currency === "EUR"){
+            price = data.bpi.EUR.rate_float;
+            console.log(price);
+        }else {
+            price = data.bpi.USD.rate_float;
+            console.log(price);
+        }
+
+        let value = price*number;
+        console.log(value);
+
+        res.send(`You have ${Math.round(value)} in ${currency}.`);
+        
+    });
+  
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+    
+});
