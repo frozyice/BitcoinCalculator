0e676c5fbfe2a0d7603b168eaca49efa50d2de4a
Karmo <Karmo.Kiima@Eesti.ee>
2020-03-24 18:28:46 +0200

    Add Heroku

--- server.js
+++ server.js
@@ -36,7 +36,6 @@ app.post("/", function(req, res){
   
 });
 
-app.listen(3000, function(){
-    console.log("Server is running on port 3000");
-    
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server has started.");
 });
